ebc1778992eabdd39459c5b6d69cd2bb6b406ba3
Wentonn <64532578+Wentonn@users.noreply.github.com>
2021-10-30 01:23:18 +0700

    Add files via upload

---
+++ WEBSITE/gatau.js
@@ -0,0 +1,24 @@
+const box = document.getElementById("spider")
+const clickHandler = ()=>{
+    box.classList.add("show");
+}
+
+const box2 = document.getElementById("IO")
+const clickHandler2 = ()=>{
+    box2.classList.add("show");
+}
+
+const box3 = document.getElementById("coco")
+const clickHandler3 = ()=>{
+    box3.classList.add("show");
+}
+
+const box4 = document.getElementById("TS2")
+const clickHandler4 = ()=>{
+    box4.classList.add("show");
+}
+
+const box5 = document.getElementById("TS4")
+const clickHandler5 = ()=>{
+    box5.classList.add("show");
+}
